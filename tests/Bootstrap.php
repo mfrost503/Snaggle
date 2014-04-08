39ee50ff9c0751dd5a296c10d360ba6b145e3865
Matt Frost <mfrost.design@gmail.com>
2014-04-08 12:15:35 +0000

    Reconfiguring the folder structure

---
+++ tests/Bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/oauth');
+require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
---
+++ tests/src/OAuth1/Client/Signature/HmacSha1Test.php
@@ -0,0 +1,39 @@
+<?php
+namespace Snaggle\OAuth1\Client\Signature;
+use Snaggle\OAuth1\Client\Credentials as Credentials;
+
+class HmacSha1Test extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+    }
+
+    /**
+     * Tear Down
+     */
+    public function tearDown()
+    {
+    }
+
+    /**
+     * @test
+     * Test to ensure the nonce is being set correctly
+     */
+    public function generateNonce()
+    {
+        $consumer = new \Credentials\ConsumerCredentials();
+        $consumer->setIdentifier('ABCDEFG');
+        $consumer->setSecret('SHHHHHHH');
+
+        $user = new \Credentials\AccessCredentials();
+        $user->setIdentifier('1234ABCD');
+        $user->setSecret('KEEPOUT');
+
+        $signature = new HmacSha1($consumer, $user);
+        $nonce = $signature->getNonce();
+        $this->assertNotEmpty($nonce);
+    }
+}
