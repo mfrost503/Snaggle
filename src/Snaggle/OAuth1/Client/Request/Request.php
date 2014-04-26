18538d40f708d7546636ce5578b3f66b30266546
Matt Frost <mfrost.design@gmail.com>
2014-04-26 21:07:15 +0000

    Getting a start on the request component

---
+++ src/Snaggle/OAuth1/Client/Request/Request.php
@@ -0,0 +1,24 @@
+<?php
+namespace Snaggle\OAuth1\Client\Request;
+/**
+ * @author Matt Frost <mfrost.design@gmail.com>
+ * @copyright Copyright (c) 2014
+ * @license http://opensource.org/licenses/MIT
+ * @package Snaggle
+ * @subpackage OAuth1
+ *
+ * Class that will use an HTTP client to make requests for token exchanges and api consumption
+ */
+class Request
+{
+    /**
+     * Guzzle Client used for making the requests
+     *
+     * @var \GuzzleHttp\Client $httpClient
+     */
+    protected $httpClient;
+
+    /**
+     *
+     */
+}
