416613539ca0de8eb439a6879d8c0c7860e08a09
Andreas Möller <am@localheinz.com>
2015-01-16 22:02:41 +0100

    Enhancement: Add test to prove autoloading fails

---
+++ tests/Client/Credentials/TemporaryCredentialsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Snaggle\Tests\Client\Credentials;
+
+use PHPUnit_Framework_TestCase;
+
+class TemporaryCredentialsTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanAutoloadClass()
+    {
+        $this->assertTrue(class_exists('Snaggle\Oauth1\Client\Credentials\TemporaryCredentials'));
+    }
+}
